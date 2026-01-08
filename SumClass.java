d4f732621c1379f88a9e73bf636e960f5b394682
Sabbir Mahatab <sabbir107031@gmail.com>
2026-01-08 09:32:37 +0600

    SumClass.java

---
+++ SumClass.java
@@ -0,0 +1,16 @@
+package ict._22.ClassNames;
+
+public class SumClass {
+
+    public static SumClass instance = new SumClass();
+
+    private SumClass() {}
+
+    public double calculateSum() {
+        double sum = 0.0;
+        for (double i = 1.0; i >= 0.1; i -= 0.1) {
+            sum += i;
+        }
+        return sum;
+    }
+}
