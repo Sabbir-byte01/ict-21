49e581a39e8990b6aa98f1c8cc19183d9f8b4e44
Sabbir Mahatab <sabbir107031@gmail.com>
2026-01-27 22:35:40 +0600

    MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,1 +1,16 @@
 
+public class MainClass {
+public static void main(String[] args) {
+// Sum
+double result = SumClass.instance.calculateSum();
+CustomPrintClass.instance.pr("Sum = " + result);
+// GCD & LCM
+int a = 12, b = 18;
+CustomPrintClass.instance.pr("GCD = " + DivisorMultipleClass.instance.gcd(a, b));
+CustomPrintClass.instance.pr("LCM = " + DivisorMultipleClass.instance.lcm(a, b));
+// Number Conversion
+NumberConversionClass.instance.convert(25);
+// Custom Print
+CustomPrintClass.instance.pr("All classes executed successfully!");
+}
+}
